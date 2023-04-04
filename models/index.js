590dbf17800cecfb7338e03cb783e5b5142d8bf0
Noobly JS <119395465+nooblyjs@users.noreply.github.com>
2023-04-04 09:29:28 +0000

    Removed models that are meant to be in application

--- models/index.js
+++ models/index.js
@@ -12,15 +12,6 @@ module.exports = function (moduleManager) {
     // Initialise the view manager    
     _modelsManager.initialise = function () {
 
-        // Load the models
-        moduleManager.core.common.modules.loadChildModules(moduleManager.core, 'models', './models/location/country' );
-
-        // The Location Model    
-        moduleManager.core.models.location = require('./location')(moduleManager);
-
-        // The Person Model 
-        moduleManager.core.models.person = require('./person')(moduleManager);
-
     }();
 
     return _modelsManager;
